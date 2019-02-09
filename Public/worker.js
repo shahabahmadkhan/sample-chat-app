2663a4df30f5b2c8a7400e121f6479b27df94c3c
Shahab Ahmad Khan <admin@shahabahmadkhan.com>
2019-02-09 17:47:38 +0530

    added favicon and updated http to https image for worker.js

--- Public/worker.js
+++ Public/worker.js
@@ -5,6 +5,6 @@ self.addEventListener('push', ev => {
     console.log('Got push', data);
     self.registration.showNotification(data.title, {
         body: data.msgFromServer,
-        icon: 'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
+        icon: 'https://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
     });
 });
